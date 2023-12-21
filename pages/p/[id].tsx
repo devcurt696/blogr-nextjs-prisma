c044d32772a6139572b881de1e94530ad758d810
curtcurt69 <cecrouse96@yahoo.com>
2023-12-20 19:11:59 -0500

    ssss

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -3,9 +3,9 @@ import { GetServerSideProps } from "next"
 import ReactMarkdown from "react-markdown"
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
-import prisma from "../../lib/prisma"
+//import prisma from "../../lib/prisma"
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+/*export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
       id: String(params?.id),
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   return {
     props: post,
   }
-}
+}*/
 
 const Post: React.FC<PostProps> = (props) => {
   let title = props.title
@@ -32,7 +32,9 @@ const Post: React.FC<PostProps> = (props) => {
       <div>
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
-        <ReactMarkdown children={props.content} />
+        <ReactMarkdown>
+          {props.content}
+        </ReactMarkdown>
       </div>
       <style jsx>{`
         .page {
